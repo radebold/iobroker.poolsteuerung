e9f3b0d93b20fe2efd57fd26c1c643d86999f3fd
Thomas <radebold@gmail.com>
2026-03-09 21:40:08 +0100

    002

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Poolsteuerung Adapter
-Dropdown Auswahl für Sensoren und Aktoren
+
+# iobroker.poolsteuerung
+
+Version 0.0.2
+
+- Dropdown-Auswahl für Sensoren und Aktoren
+- Standardwerte aus vorhandenen Skripten
+- sichtbares Icon
+- stabiler Start des Adapters
--- main.js
+++ main.js
@@ -10,10 +10,53 @@ class Poolsteuerung extends utils.Adapter {
         });
 
         this.on('ready', this.onReady.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+        this.on('stateChange', this.onStateChange.bind(this));
     }
 
     async onReady() {
         this.log.info('poolsteuerung adapter started');
+        await this.setObjectNotExistsAsync('info.connection', {
+            type: 'state',
+            common: {
+                name: 'Connection',
+                type: 'boolean',
+                role: 'indicator.connected',
+                read: true,
+                write: false,
+                def: false
+            },
+            native: {}
+        });
+        await this.setObjectNotExistsAsync('status.overall.message', {
+            type: 'state',
+            common: {
+                name: 'Overall status message',
+                type: 'string',
+                role: 'text',
+                read: true,
+                write: false,
+                def: ''
+            },
+            native: {}
+        });
+        await this.setStateAsync('info.connection', true, true);
+        await this.setStateAsync('status.overall.message', 'Adapter gestartet - Version 0.0.2', true);
+        this.subscribeStates('*');
+    }
+
+    async onUnload(callback) {
+        try {
+            await this.setStateAsync('info.connection', false, true);
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    onStateChange(id, state) {
+        if (!state || state.ack) return;
+        this.log.debug(`stateChange ${id}: ${state.val}`);
     }
 }
 
