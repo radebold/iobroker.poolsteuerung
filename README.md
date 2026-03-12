ac336813c869d5d1fca51132a377698b0800c1e4
Thomas <radebold@gmail.com>
2026-03-12 16:36:56 +0100

    002

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 
 Version 0.0.2
 
-- Dropdown-Auswahl für Sensoren und Aktoren
-- Standardwerte aus vorhandenen Skripten
-- sichtbares Icon
-- stabiler Start des Adapters
+Fix:
+- Sensoren und Aktoren nutzen jetzt `objectId` statt `state`
+- Dadurch werden die Auswahlfelder in Admin korrekt angezeigt
--- main.js
+++ main.js
@@ -1,67 +1,19 @@
 
 'use strict';
-const utils = require('@iobroker/adapter-core');
+const utils=require('@iobroker/adapter-core');
 
-class Poolsteuerung extends utils.Adapter {
-    constructor(options) {
-        super({
-            ...options,
-            name: 'poolsteuerung'
-        });
-
-        this.on('ready', this.onReady.bind(this));
-        this.on('unload', this.onUnload.bind(this));
-        this.on('stateChange', this.onStateChange.bind(this));
-    }
-
-    async onReady() {
-        this.log.info('poolsteuerung adapter started');
-        await this.setObjectNotExistsAsync('info.connection', {
-            type: 'state',
-            common: {
-                name: 'Connection',
-                type: 'boolean',
-                role: 'indicator.connected',
-                read: true,
-                write: false,
-                def: false
-            },
-            native: {}
-        });
-        await this.setObjectNotExistsAsync('status.overall.message', {
-            type: 'state',
-            common: {
-                name: 'Overall status message',
-                type: 'string',
-                role: 'text',
-                read: true,
-                write: false,
-                def: ''
-            },
-            native: {}
-        });
-        await this.setStateAsync('info.connection', true, true);
-        await this.setStateAsync('status.overall.message', 'Adapter gestartet - Version 0.0.2', true);
-        this.subscribeStates('*');
-    }
-
-    async onUnload(callback) {
-        try {
-            await this.setStateAsync('info.connection', false, true);
-            callback();
-        } catch (e) {
-            callback();
-        }
-    }
-
-    onStateChange(id, state) {
-        if (!state || state.ack) return;
-        this.log.debug(`stateChange ${id}: ${state.val}`);
-    }
+class poolsteuerung extends utils.Adapter{
+ constructor(options){
+  super({...options,name:'poolsteuerung'});
+  this.on('ready',this.onReady.bind(this));
+ }
+ async onReady(){
+  this.log.info('poolsteuerung started');
+ }
 }
 
-if (require.main !== module) {
-    module.exports = options => new Poolsteuerung(options);
-} else {
-    new Poolsteuerung();
+if(require.main!==module){
+ module.exports=options=>new poolsteuerung(options);
+}else{
+ new poolsteuerung();
 }
