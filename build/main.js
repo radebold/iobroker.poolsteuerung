3dad0a4128424a797054b3ef8bdcffe7842bb3c9
Thomas <radebold@gmail.com>
2026-03-16 23:02:06 +0100

    1

--- build/main.js
+++ build/main.js
@@ -1,1 +1,37 @@
-'use strict';\nconst utils=require('@iobroker/adapter-core');\nclass Poolsteuerung extends utils.Adapter{constructor(options={}){super({...options,name:'poolsteuerung'});this.on('ready',this.onReady.bind(this));}async onReady(){this.log.info('poolsteuerung 0.1.1-final started');}}\nif(require.main!==module){module.exports=options=>new Poolsteuerung(options);}else{(()=>new Poolsteuerung())();}\n
+
+'use strict';
+const utils = require('@iobroker/adapter-core');
+
+class Poolsteuerung extends utils.Adapter {
+
+    constructor(options = {}) {
+        super({
+            ...options,
+            name: 'poolsteuerung'
+        });
+        this.on('ready', this.onReady.bind(this));
+    }
+
+    async onReady() {
+        this.log.info('poolsteuerung 0.1.3 started');
+
+        const debug = this.config.debug;
+
+        if (debug) {
+            this.log.info('[DEBUG] Adapter gestartet');
+        }
+
+        // Beispiel Zyklus
+        setInterval(async () => {
+            if (debug) this.log.info('[DEBUG] cycle start');
+
+        }, 60000);
+
+    }
+}
+
+if (require.main !== module) {
+    module.exports = (options) => new Poolsteuerung(options);
+} else {
+    (() => new Poolsteuerung())();
+}
