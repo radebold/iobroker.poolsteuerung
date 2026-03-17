0c94b7dcf4a0c3cb8c2c3a95177adf48f1861868
Thomas <radebold@gmail.com>
2026-03-17 20:46:44 +0100

    030

--- build/main.js
+++ build/main.js
@@ -1,9 +1,63 @@
+
 'use strict';
 const utils = require('@iobroker/adapter-core');
-function toNumber(val){ if (val === null || val === undefined) return 0; return Number(String(val).replace(',', '.')); }
-class Poolsteuerung extends utils.Adapter {
-  constructor(options={}){ super({...options,name:'poolsteuerung'}); this.on('ready',this.onReady.bind(this)); }
-  calcVolume(){ const d=toNumber(this.config.poolDiameterM); const h=toNumber(this.config.poolWaterHeightM); if(!d||!h) return 0; return Number((Math.PI*Math.pow(d/2,2)*h).toFixed(2)); }
-  async onReady(){ const v=this.calcVolume(); this.log.info('poolsteuerung 0.2.6-clean started'); this.log.info('Berechnetes Poolvolumen: '+v+' m3'); }
+
+function parseNum(v){
+    if(v === undefined || v === null) return 0;
+    return Number(String(v).replace(',', '.'));
 }
-if(require.main!==module){ module.exports=options=>new Poolsteuerung(options); } else { (()=>new Poolsteuerung())(); }
+
+class Poolsteuerung extends utils.Adapter {
+
+    constructor(options = {}) {
+        super({...options, name: 'poolsteuerung'});
+        this.on('ready', this.onReady.bind(this));
+    }
+
+    async calculateVolume(){
+
+        const d = parseNum(this.config.poolDiameterM);
+        const h = parseNum(this.config.poolWaterHeightM);
+
+        if(!d || !h){
+            this.log.warn("Durchmesser oder Höhe nicht gesetzt");
+            return;
+        }
+
+        const volume = Math.PI * Math.pow(d/2,2) * h;
+        const result = Number(volume.toFixed(2));
+
+        await this.setObjectNotExistsAsync("info.poolVolume", {
+            type: "state",
+            common:{
+                name:"Poolvolumen",
+                type:"number",
+                role:"value.volume",
+                unit:"m3",
+                read:true,
+                write:false
+            },
+            native:{}
+        });
+
+        await this.setStateAsync("info.poolVolume", {
+            val: result,
+            ack:true
+        });
+
+        this.log.info("Poolvolumen berechnet: " + result + " m3");
+
+    }
+
+    async onReady(){
+
+        await this.calculateVolume();
+
+    }
+}
+
+if (require.main !== module) {
+    module.exports = options => new Poolsteuerung(options);
+} else {
+    (() => new Poolsteuerung())();
+}
