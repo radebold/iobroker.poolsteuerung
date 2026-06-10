8d4edf3da42afb019b4c36fc017350660e706b09
Thomas <radebold@gmail.com>
2026-06-10 11:24:45 +0200

    hf52

--- build/main.js
+++ build/main.js
@@ -1168,7 +1168,7 @@ body{margin:0;background:radial-gradient(circle at top left, rgba(89,188,255,.18
       heatpumpStateId: this.config.heatpumpPowerStateId || '',
       heatpumpSetTempStateId: this.config.heatpumpSetTempStateId || '',
       phManualDoseSec: await this.getText('poolsteuerung.0.control.ph.manualDoseSec', '30'),
-      adapterVersion: 'v0.3.15hf51'
+      adapterVersion: 'v0.3.15hf52'
     };
 
     const now = Date.now();
@@ -2225,6 +2225,7 @@ body{margin:0;background:radial-gradient(circle at top left, rgba(89,188,255,.18
           await this.setSwitchStateCompat(this.config.circulationPumpSocketStateId, !!state.val);
         }
         if (id === `${this.namespace}.control.device.chlorinator`) {
+          await this.setStateIfChanged('control.auto.chlor', false, false);
           await this.setSwitchStateCompat(this.config.chlorinatorSocketStateId, !!state.val);
         }
         if (id === `${this.namespace}.control.device.phPump`) {
