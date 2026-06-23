b97ec4b281d19b399ee569f9a4df4b769c03be9c
Thomas <radebold@gmail.com>
2026-06-23 23:33:47 +0200

    25

--- build/main.js
+++ build/main.js
@@ -1583,7 +1583,7 @@ body{margin:0;background:radial-gradient(circle at top left, rgba(89,188,255,.18
       heatpumpFanPercent,
       heatpumpMode,
       phManualDoseSec: await this.getText('poolsteuerung.0.control.ph.manualDoseSec', '30'),
-      adapterVersion: 'v0.3.16hf24'
+      adapterVersion: 'v0.3.16hf25'
     };
 
     const now = Date.now();
