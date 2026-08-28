f5f5d5ae730ca55c6816a46d7670157209fe109f
Thomas <radebold@gmail.com>
2026-08-28 19:55:32 +0200

    Bump io-package to 0.5.71

---
+++ scripts/patch-objects-571.js
@@ -0,0 +1,12 @@
+'use strict';
+const fs=require('fs');
+const path=require('path');
+const root=path.resolve(__dirname,'..');
+require('./patch-objects-570.js');
+const ioFile=path.join(root,'io-package.json');
+const io=JSON.parse(fs.readFileSync(ioFile,'utf8'));
+io.version='0.5.71';
+io.common=io.common||{};
+io.common.version='0.5.71';
+fs.writeFileSync(ioFile,JSON.stringify(io,null,2)+'\n');
+console.log('[0.5.71] Phone-Temperaturkurve direkt aus vorhandener SVG in die Temperaturzeile kopiert; Gesamtgroesse unveraendert');
