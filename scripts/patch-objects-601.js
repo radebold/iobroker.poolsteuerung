815fc6eafcdf606c963dc7703624824c60761716
Thomas <radebold@gmail.com>
2026-09-16 17:24:41 +0300

    0.6.1 postinstall standby fix

---
+++ scripts/patch-objects-601.js
@@ -0,0 +1,12 @@
+'use strict';
+const fs=require('fs');
+const path=require('path');
+const root=path.resolve(__dirname,'..');
+require('./patch-objects-600.js');
+const ioFile=path.join(root,'io-package.json');
+const io=JSON.parse(fs.readFileSync(ioFile,'utf8'));
+io.version='0.6.1';
+io.common=io.common||{};
+io.common.version='0.6.1';
+fs.writeFileSync(ioFile,JSON.stringify(io,null,2)+'\n');
+console.log('[0.6.1] Standby aus Admin-Konfiguration wird zuverlässig in Laufzeit, Aktoren und VIS übernommen. Single-VIS-Owner aus 0.6.0 bleibt unverändert.');
